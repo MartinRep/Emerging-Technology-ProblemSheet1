8c758ebb023c1e07f01d849f2fec2d85fd5d3e6a
repickym <repickym@gmail.com>
2017-09-26 20:45:00 +0100

    SquareRoot modified to stop when Z is not changing more then defined in margin variable

--- 09-SquareRoot.py
+++ 09-SquareRoot.py
@@ -1,9 +1,22 @@
-def SquareRoot(x):
+import math
+def SquareRoot(x, margin):
     z = 1
+    #Sets the last Z calculated
     lastZ = None
-    while z != lastZ:
+    #Initialize the while condition value
+    found = False
+    #While loop until Z and previous Z are equal with margin in mind
+    while found == False:
+        #Update the lastZ with current Z value
         lastZ = z
+        #Calculate the next Z from Newton Square Root method
         z = z - ((z*z - x) / (2 * z))
-        print(z)
+        #print(z)
+        #Check if Z is changing more the defined in margin variable
+        if (z <= lastZ+margin) and (z >= lastZ-margin): found = True
+    return z
     
-SquareRoot(25)
+#Test
+result = SquareRoot(250,0.5)
+mathResult = math.sqrt(250)
+print("Function: "+str(result)+" Math,sqrt: "+str(mathResult)+" Difference: "+str(result - mathResult))
