7664a12a39d151e497022f9a3982dae1c588e515
repickym <repickym@gmail.com>
2017-09-26 20:09:52 +0100

    Reverse String comments

--- 10-ReverseString.py
+++ 10-ReverseString.py
@@ -1,4 +1,6 @@
+#Adapted from https://stackoverflow.com/questions/931092/reverse-a-string-in-python#931095
 def reverseString(strng):
+    #string[start:stop:step]
     return strng[::-1]
-
+#Test
 print(reverseString("Repo"))
