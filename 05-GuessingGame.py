cbb4d56cabe783c503e962ce843da8a619425829
repickym <repickym@gmail.com>
2017-09-26 18:36:10 +0100

    GuessGame finished

--- 05-GuessingGame.py
+++ 05-GuessingGame.py
@@ -1,22 +1,30 @@
-#Adapted from https://docs.scipy.org/doc/numpy/user/basics.creation.html
+#Adapted from http://www.effbot.org/pyfaq/how-do-i-generate-random-numbers-in-python.htm
+#             https://stackoverflow.com/questions/1712227/how-to-get-the-size-of-a-list#1712236
 import random as random
+#List of guesses
 guesses = []
 correntGuess = False
+#Magic number is generated as a random number between 0 and 100
 magicNumber = random.randrange(0,100)
-print(magicNumber)
+print("The magic number is number between 0 and 100")
+#for development purposes
+#print (magicNumber)
 while correntGuess != True:
     guess = input("Guess the magic number: ")    
+    #List is empty adds guess
     if guesses.__len__() == 0 :
         guesses.append(int(guess))
-        print(guesses)
+    #Checks if last guess is not the same as current one and only then the counter of guesses is updated
     elif guesses[-1] != int(guess):
         guesses.append(int(guess))
-        print(guesses)
+    #Correct quess end the while loop
     if magicNumber == int(guess):
         correntGuess = True
+    #Displays guess hint
     elif int(guess) > magicNumber:
         print("Your guess is high")
     elif int(guess) < magicNumber:
         print("Your guess is low")
+#After correct guess prints out congratulation and size of the list of guesses to display number of attempts
 print("That's the magic number! Congratulation!")        
 print("Number of attempts: "+str(guesses.__len__()))
