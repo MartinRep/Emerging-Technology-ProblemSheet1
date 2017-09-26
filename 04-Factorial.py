22b7c9f675fbc15224dd2d937fc7969c814a67cf
repickym <repickym@gmail.com>
2017-09-26 16:46:35 +0100

    Sum of Digits added

--- 04-Factorial.py
+++ 04-Factorial.py
@@ -1,8 +1,15 @@
+#Recursive function for calculating Factorial
 def factorial(x):
-    if x == 1: 
-        result = x
-        return result
-    return x*factorial(x-1)
-
+    #if end of the factorial, then starts calling back
+    if x == 1: return x
+    else: return factorial(x-1)*x
 #Test
-print factorial(10)
+sumNumber=0
+print ("Factorial of 100: "+str(factorial(100)))
+#Gets and converts the result of (int) factorial into String
+result = str(factorial(100))
+#Loops through the String char by char. Converts it to the Integer and add it to the sumNumbre variable
+for i in range(0, len(result)):
+    sumNumber += int(result[i])
+#Prints out the sum result
+print("Sum of Factorial(100): "+str(sumNumber))
