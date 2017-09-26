74bedbf1e381590ea0ba028c47c32e36ba97f508
repickym <repickym@gmail.com>
2017-09-26 18:29:14 +0100

    GuessGame working

--- 05-GuessingGame.py
+++ 05-GuessingGame.py
@@ -1,5 +1,22 @@
+#Adapted from https://docs.scipy.org/doc/numpy/user/basics.creation.html
+import random as random
 guesses = []
-right = False
-while right != True:
-    guess = input("Guess the magic number: ")
-    right = True
+correntGuess = False
+magicNumber = random.randrange(0,100)
+print(magicNumber)
+while correntGuess != True:
+    guess = input("Guess the magic number: ")    
+    if guesses.__len__() == 0 :
+        guesses.append(int(guess))
+        print(guesses)
+    elif guesses[-1] != int(guess):
+        guesses.append(int(guess))
+        print(guesses)
+    if magicNumber == int(guess):
+        correntGuess = True
+    elif int(guess) > magicNumber:
+        print("Your guess is high")
+    elif int(guess) < magicNumber:
+        print("Your guess is low")
+print("That's the magic number! Congratulation!")        
+print("Number of attempts: "+str(guesses.__len__()))
